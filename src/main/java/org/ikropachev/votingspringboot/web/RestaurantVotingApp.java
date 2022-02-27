f77a60ab5367755a1b0dde40be0d5ab86f7653ec
Rock133 <paporotnik796@gmail.com>
2022-02-27 21:35:04 +0300

    add_base_packages

---
+++ src/main/java/org/ikropachev/votingspringboot/web/RestaurantVotingApp.java
@@ -0,0 +1,12 @@
+package org.ikropachev.votingspringboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApp.class, args);
+    }
+}
