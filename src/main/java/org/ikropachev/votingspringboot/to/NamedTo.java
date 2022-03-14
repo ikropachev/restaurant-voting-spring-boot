6083b025caed39f1c94fb6fd445516bfcf216f58
Rock133 <paporotnik796@gmail.com>
2022-03-14 06:38:33 +0300

    add_NamedTo

---
+++ src/main/java/org/ikropachev/votingspringboot/to/NamedTo.java
@@ -0,0 +1,27 @@
+package org.ikropachev.votingspringboot.to;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.ikropachev.votingspringboot.util.validation.NoHtml;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class NamedTo extends BaseTo {
+    @NotBlank
+    @Size(min = 2, max = 100)
+    @NoHtml
+    protected String name;
+
+    public NamedTo(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + '[' + name + ']';
+    }
+}
