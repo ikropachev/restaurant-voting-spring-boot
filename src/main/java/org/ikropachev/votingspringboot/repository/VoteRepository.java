36b2861562ae82c9175e94d937ecb87a190f8bd9
Rock133 <paporotnik796@gmail.com>
2022-05-08 23:39:38 +0300

    add_vote_repository

---
+++ src/main/java/org/ikropachev/votingspringboot/repository/VoteRepository.java
@@ -0,0 +1,20 @@
+package org.ikropachev.votingspringboot.repository;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.ikropachev.votingspringboot.model.Vote;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Transactional(readOnly = true)
+@Tag(name = "Vote Controller")
+public interface VoteRepository extends BaseRepository<Vote>{
+    @Query("SELECT v FROM Vote v ORDER BY v.date DESC")
+    List<Vote> findAll();
+
+    @Query("SELECT v FROM Vote v WHERE v.userId=:userId AND v.date=:date")
+    Vote getByUserIdAndDate(@Param("userId") int userId, @Param("date") LocalDate date);
+}
